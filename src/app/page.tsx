e0b896cf1a7b5c66950bef3ceaffe4d4846882f0
blaisepascal287 <130232913+blaisepascal287@users.noreply.github.com>
2024-04-09 21:22:59 +0530

    Built homepage and solved middleware file bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen flex flex-col items-center justify-center p-2">
+      <h1 className="text-3xl my-4">
+
       Homepage
+      </h1>
+      <Link href="/login" className="underline hover:no-underline">
+        Login
+      </Link>
+      <Link href="/signup" className="underline hover:no-underline">
+        Signup
+      </Link>
     </main>
   );
 }
